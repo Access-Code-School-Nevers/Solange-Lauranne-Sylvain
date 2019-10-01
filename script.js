399f681af0fbad86e8c52bb4e9bcfa0fc6f4ce97
Lauranne <lauranne.marcel@gmail.com>
2019-10-01 09:36:39 +0200

    setup html structure

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-var token = document.getElementById('token');
+var player = document.getElementById('player');
 var moveSize = 50;
 var parentRect = document.getElementById('board').getBoundingClientRect();
 var childRect;
 
-// Handle arrows to move the token
+// Handle arrows to move the player
 document.addEventListener('keydown', function(event) {
-  childRect = document.getElementById('token').getBoundingClientRect();
+  childRect = document.getElementById('player').getBoundingClientRect();
 
   if (event.code == 'ArrowUp') {
     if(childRect.top > parentRect.top)
-      token.style.top = (token.offsetTop - moveSize)+"px";
+      player.style.top = (player.offsetTop - moveSize)+"px";
   }
   else if (event.code == 'ArrowRight') {
     if(childRect.right < parentRect.right)
-      token.style.left = (token.offsetLeft + moveSize)+"px";
+      player.style.left = (player.offsetLeft + moveSize)+"px";
   }
   else if (event.code == 'ArrowDown') {
     if(childRect.bottom < parentRect.bottom)
-      token.style.top = (token.offsetTop + moveSize)+"px";
+      player.style.top = (player.offsetTop + moveSize)+"px";
   }
   else if (event.code == 'ArrowLeft') {
     if(childRect.left > parentRect.left)
-      token.style.left = (token.offsetLeft - moveSize)+"px";
+      player.style.left = (player.offsetLeft - moveSize)+"px";
   }
 });
