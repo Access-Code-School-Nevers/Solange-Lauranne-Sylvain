884876911cc80f28aa94442c5431eed4e57fa5fe
Lauranne <lauranne.marcel@gmail.com>
2019-10-01 14:45:44 +0200

    fix bomb

--- bomb.js
+++ bomb.js
@@ -3,16 +3,13 @@ document.onload =  hide(bomb);
 let bombActivated = false;
 
 document.addEventListener('keydown', (event) => {
-  console.log(event.code);
-  console.log(bombActivated);
   if (event.code == 'Numpad0' && bombActivated == false) {
     let player = document.getElementById('player');
-    console.log(player);
     bomb.style.top = player.offsetTop + 'px';
     bomb.style.left = player.offsetLeft + 'px';
     show(bomb);
     bombActivated = true;
-    bombTimerStart()
+    bombTimerStart();
   }
 });
 
@@ -31,8 +28,10 @@ function hide(elem){
 function bombTimerStart() {
   //Décompter un timer
   explodeBomb();
+  // bombActivated = false;
+  // hide(bomb);
 }
 
 function explodeBomb() {
-  
+  //détruit les trucs dans les cases autour
 }
