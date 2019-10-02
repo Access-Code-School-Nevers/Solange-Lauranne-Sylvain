4a66f2ad748db32197dfd794b102598171b47160
Lauranne <lauranne.marcel@gmail.com>
2019-10-02 17:09:02 +0200

    defeat condition

--- bomb.js
+++ bomb.js
@@ -3,35 +3,54 @@ document.onload =  hide(bomb);
 let bombActivated = false;
 
 document.addEventListener('keydown', (event) => {
-  if (event.code == 'Numpad0' && bombActivated == false) {
+  if ((event.code == 'Numpad0' || event.code == 'Space') && bombActivated == false) {
     let player = document.getElementById('player');
     bomb.style.top = player.offsetTop + 'px';
     bomb.style.left = player.offsetLeft + 'px';
     show(bomb);
     bombActivated = true;
-    bombTimerStart();
+    setTimeout(explodeBomb, 1500)
   }
 });
 
 function show(elem) {
-  // elem.style.display = 'block';
-  elem.classList.add('visible');
-  elem.classList.remove('not-visible');
+  elem.style.display = 'block';
 }
 
 function hide(elem){
-  // elem.style.display = 'none';
-  elem.classList.add('not-visible');
-  elem.classList.remove('visible');
-}
-
-function bombTimerStart() {
-  //Décompter un timer
-  explodeBomb();
-  // bombActivated = false;
-  // hide(bomb);
+  elem.style.display = 'none';
 }
 
 function explodeBomb() {
-  //détruit les trucs dans les cases autour
+  let blast = document.getElementsByClassName ('blast');
+  for(i = 0; i< blast.length; i++) {
+    show(blast.item(i));
+  }
+  let [x, y] = getSquare(bomb);
+  console.log([x, y]);
+  for (let i = x-1; i <= x+1; i++){
+    for (let j = y-1; j<= y+1; j++) {
+      console.log([i, j]);
+      if (i >= 0 && j >= 0 && i < 25 && j < 25 && !!grid[i][j]) {
+        elem = document.getElementById(grid[i][j]);
+        if (grid[i][j] == 'player') {
+          let replay = confirm('GAME OVER \n Do you want to play a game ?');
+          if (replay) {
+            location.reload();
+          }
+        }
+        grid[i][j] = undefined;
+        hide(elem);
+      }
+    }
+  }
+  setTimeout(function(){
+    hide(bomb);
+    for(i = 0; i< blast.length; i++) {
+      hide(blast.item(i));
+    }
+    bombActivated = false;
+  }, 1000);
+
+  console.log(grid);
 }
--- script.js
+++ script.js
@@ -1,57 +1,132 @@
+// CONTANTS //
+const moveSize = 32;
+
+// INITIALIZE GRID //
+let grid = Array(25);
+for (let x = 0; x < 25; x++){
+  grid[x] = Array(25);
+}
 
 
-var player = document.getElementById('player');
-var moveSize = 32;
-var parentRect = document.getElementById('board').getBoundingClientRect();
-var childRect;
+// INITIALIZE PLAYER //
+let player = document.getElementById('player');
+grid[0][0] = 'player';
 
-// PLAYER MOVEMENT //
+// INITIALIZE ENNEMIS AND WALLS //
+let badguys = document.getElementsByClassName ("badguy");
+window.onload = function() {
+  for (i = 0; i < badguys.length; i++){
+    let badguy = badguys.item(i);
+    let [x, y] = getRandomPosition();
+    badguy.setAttribute('id', 'badguy'+i)
+    create (badguy, "badguy"+i, x, y);
+  }
+  let walls = document.getElementById ("walls");
+  // console.log (wall)
+  for (let i = 0; i < 150; i++){
+    let wall = document.createElement ("div");
+    walls.appendChild(wall);
+    wall.classList.add ("wall");
+    wall.setAttribute('id', 'wall'+i)
+    let positionned = false;
+    while (!positionned) {
+      let [x, y] = getRandomPosition();
+      positionned = create(wall, 'wall'+i, x, y);
+    }
+  }
+}
+
+// ENNEMI MOVEMENT //
+setInterval(function() {
+  for (i = 0; i< badguys.length; i++) {
+    let direction = Math.floor(Math.random()*4);
+    switch (direction) {
+      case 1 : moveElem(badguys.item(i), "badguy"+i, 1, 0); break;
+      case 2 : moveElem(badguys.item(i), "badguy"+i, -1, 0); break;
+      case 3 : moveElem(badguys.item(i), "badguy"+i, 0, 1); break;
+      case 0 : moveElem(badguys.item(i), "badguy"+i, 0, -1); break;
+    }
+    checkSides(badguys.item(i), 'player');
+  }
+}, 1000)
+
+// PLAYER MOVEMENT ON ARROW KEY //
 document.addEventListener('keydown', function(event) {
-
-  childRect = document.getElementById('player').getBoundingClientRect();
-  // console.log(event.code);
   if (event.code == 'ArrowUp') {
-    if(childRect.top > parentRect.top)
-      player.style.top = (player.offsetTop - moveSize)+"px";
+      moveElem(player, 'player', 0, -1);
   }
   else if (event.code == 'ArrowRight') {
-    if(childRect.right < parentRect.right - moveSize)
-      player.style.left = (player.offsetLeft + moveSize)+"px";
+      moveElem(player, 'player', 1, 0);
   }
   else if (event.code == 'ArrowDown') {
-    if(childRect.bottom < parentRect.bottom - moveSize)
-      player.style.top = (player.offsetTop + moveSize)+"px";
+      moveElem(player, 'player', 0, 1);
   }
   else if (event.code == 'ArrowLeft') {
-    if(childRect.left > parentRect.left)
-      player.style.left = (player.offsetLeft - moveSize)+"px";
+      moveElem(player, 'player', -1, 0);
   }
+  checkSides(player, 'badguy');
 });
 
-// RANDOM ENNEMI //
 function getRandomPosition() {
-	var randomX = Math.floor(Math.random()*(25));
-	var randomY = Math.floor(Math.random()*(25));
-  console.log ([randomX,randomY])
+	let randomX = Math.floor(Math.random()*(25));
+	let randomY = Math.floor(Math.random()*(25));
+  // console.log ([randomX,randomY])
 	return [randomX,randomY];
 }
-window.onload = function() {
-	var badguys = document.getElementsByClassName ("badguy");
-  console.log (badguys)
-  for (i = 0; i < badguys.length; i++){
-    randomPos(badguys.item(i));
+
+function create(elem, elemName, x, y) {
+  if (!grid[x][y]){
+    grid[x][y] = elemName;
+    elem.style.top = y * 32 + 'px';
+    elem.style.left = x * 32 + 'px';
+    return true;
   }
-  var walls = document.getElementById ("walls");
-  // console.log (wall)
-  for (let i = 0; i < 50; i++){
-  let wall = document.createElement ("div");
-  walls.appendChild(wall)
-  wall.classList.add ("wall")
-  randomPos(wall);
+  else {
+    return false;
   }
 }
-function randomPos(elem) {
-  var xy = getRandomPosition();
-  elem.style.top = xy[0] * 32 + 'px';
-  elem.style.left = xy[1] * 32 + 'px';
+
+function moveElem(elem, elemName, xMove, yMove) {
+  let [x, y] = getSquare(elem);
+  let fx = x + xMove;
+  let fy = y + yMove;
+  //check is destination is empty
+  if(fx >= 0 && fx < 25 && fy >= 0 && fy < 25 && !grid[fx][fy]){
+    //move elem in the grid
+    grid[x][y] = undefined;
+    grid[fx][fy] = elemName;
+    //move html element
+    elem.style.left = (x + xMove) * 32 + 'px';
+    elem.style.top = (y + yMove) * 32 + 'px';
+    return true;
+  }
+  else  { // impossible move
+    return false;
+  }
+}
+
+// renvoie la position dans la grille d'un élément
+function getSquare(elem) {
+  let topPx = Math.floor(elem.offsetTop);
+  let leftPx = Math.floor(elem.offsetLeft);
+  return [Math.floor(leftPx/32), Math.floor(topPx/32)];
+}
+
+function checkSides(elem, elemType) {
+  console.log(elem);
+  let [x, y] = getSquare(elem);
+  for (let i = x-1; i <= x+1; i++){
+    for (let j = y-1; j<= y+1; j++) {
+      if (i >= 0 && j >= 0 && i < 25 && j < 25 && !!grid[i][j]) {
+        console.log(grid[i][j]);
+        console.log(grid[i][j].indexOf(elemType));
+        if (grid[i][j].indexOf(elemType) >= 0 ) {
+          let replay = confirm('GAME OVER \n Do you want to play a game ?');
+          if (replay) {
+            location.reload();
+          }
+        }
+      }
+    }
+  }
 }
